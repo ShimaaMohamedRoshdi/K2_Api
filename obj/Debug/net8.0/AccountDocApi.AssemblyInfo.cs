88f7b72b50d9c483f45a0393a11c83d896db8c55
Shimaa Mohamed <20212428@fue.edu.eg>
2026-09-21 16:40:09 +0300

    update in memo data

--- obj/Debug/net8.0/AccountDocApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/AccountDocApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AccountDocApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09ab215af064840ec80f91860d4611ecaeecbbf3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff802e3eded0d85f266809e3f9d9a989f0bfcf0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AccountDocApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AccountDocApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
